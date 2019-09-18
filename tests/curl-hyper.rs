ebcba1e460bd14da601713dd45087fe65a7e7133
Austin Bonander <austin@launchbadge.com>
2019-09-18 16:27:46 -0700

    fix curl-hyper test

--- tests/curl-hyper.rs
+++ tests/curl-hyper.rs
@@ -6,17 +6,16 @@ use http::{Method, HeaderMap, StatusCode, Request};
 use hyper::{Response, Server, Body};
 use hyper::service::{make_service_fn, service_fn};
 
-use futures::{FutureExt, TryStreamExt};
+use futures::{future, FutureExt, StreamExt, TryStreamExt};
 
 use multipart_async::server::Multipart;
-use curl::easy::{Handler, Easy2, WriteError, Form};
+use curl::easy::{Handler, Easy2, WriteError, Form, ReadError};
 use std::thread;
 use std::ops::Range;
 
-struct DummyHandler;
+struct TestHandler;
 
-impl Handler for DummyHandler {
-}
+impl Handler for TestHandler {}
 
 #[tokio::test]
 async fn test_hyper_with_curl() {
@@ -25,67 +24,77 @@ async fn test_hyper_with_curl() {
 
     let builder = Server::bind(&addr);
 
+    let (tx, mut rx) = futures::channel::mpsc::channel::<()>(0);
+
     // And a MakeService to handle each connection...
-    let make_service = make_service_fn(|_| async {
-        Ok::<_, hyper::Error>(service_fn(|req: Request<Body>| async {
-            assert_eq!(req.uri().path(), "/multipart-upload");
+    let make_service = make_service_fn(move |_| {
+        let tx = tx.clone();
 
-            let mut multipart = Multipart::try_from_request(req).unwrap();
-            let mut normal_done = false;
-            let mut text_done = false;
-            let mut binary_done = false;
+        future::ok::<_, hyper::Error>(service_fn(move |req: Request<Body>| {
+            let mut tx = tx.clone();
 
-            while let Some(mut field) = multipart.next_field().await.unwrap() {
-                match &field.headers.name[..] {
-                    "normal" => {
-                        assert!(!normal_done);
-                        normal_done = true;
+            async move {
+                assert_eq!(req.uri().path(), "/multipart-upload");
 
-                        assert!(field.headers.is_text());
-                        assert_eq!(field.headers.filename, None);
-                        assert_eq!(field.headers.content_type, None);
-                        assert_eq!(field.headers.ext_headers, HeaderMap::new());
-                        assert_eq!(field.data.read_to_string().await.unwrap(), "Hello, world!");
-                    }
-                    "text-field" => {
-                        assert!(!text_done);
-                        text_done = true;
+                let mut multipart = Multipart::try_from_request(req).unwrap();
+                let mut normal_done = false;
+                let mut text_done = false;
+                let mut binary_done = false;
 
-                        assert_eq!(field.headers.filename, Some("text-file.txt".to_string()));
-                        assert!(field.headers.is_text());
-                        assert_eq!(field.headers.content_type, Some(mime::TEXT_PLAIN));
-                        assert_eq!(field.headers.ext_headers, HeaderMap::new());
-                        assert_eq!(field.data.read_to_string().await.unwrap(),
-                                   "Hello, world from a text file!");
-                    }
-                    "binary-field" => {
-                        assert!(!binary_done);
-                        binary_done = true;
+                while let Some(mut field) = multipart.next_field().await.unwrap() {
+                    match &field.headers.name[..] {
+                        "normal" => {
+                            assert!(!normal_done);
+                            normal_done = true;
 
-                        assert_eq!(field.headers.filename, Some("binary-file.bin".to_string()));
-                        assert!(!field.headers.is_text());
-                        assert_eq!(field.headers.content_type, Some(mime::APPLICATION_OCTET_STREAM));
-                        assert_eq!(field.headers.ext_headers, HeaderMap::new());
-
-                        let mut data = 0u8 ..= 255;
-
-                        if let Some(chunk) = field.data.try_next().await.unwrap() {
-                            let mut chunk_data = chunk.iter();
-                            assert!(chunk_data.by_ref().zip(&mut data).all(|(&l, r)| l == r));
-                            assert_eq!(chunk_data.next(), None);
+                            assert!(field.headers.is_text());
+                            assert_eq!(field.headers.filename, None);
+                            assert_eq!(field.headers.content_type, None);
+                            assert_eq!(field.headers.ext_headers, HeaderMap::new());
+                            assert_eq!(field.data.read_to_string().await.unwrap(), "Hello, world!");
                         }
+                        "text-field" => {
+                            assert!(!text_done);
+                            text_done = true;
 
-                       assert_eq!(data.next(), None);
-                    },
-                    unknown => panic!("unexpected field {:?}", unknown),
+                            assert_eq!(field.headers.filename, Some("text-file.txt".to_string()));
+                            assert!(field.headers.is_text());
+                            assert_eq!(field.headers.content_type, Some(mime::TEXT_PLAIN));
+                            assert_eq!(field.headers.ext_headers, HeaderMap::new());
+                            assert_eq!(field.data.read_to_string().await.unwrap(),
+                                       "Hello, world from a text file!");
+                        }
+                        "binary-field" => {
+                            assert!(!binary_done);
+                            binary_done = true;
+
+                            assert_eq!(field.headers.filename, Some("binary-file.bin".to_string()));
+                            assert!(!field.headers.is_text());
+                            assert_eq!(field.headers.content_type, Some(mime::APPLICATION_OCTET_STREAM));
+                            assert_eq!(field.headers.ext_headers, HeaderMap::new());
+
+                            let mut data = 0u8..=255;
+
+                            if let Some(chunk) = field.data.try_next().await.unwrap() {
+                                let mut chunk_data = chunk.iter();
+                                assert!(chunk_data.by_ref().zip(&mut data).all(|(&l, r)| l == r));
+                                assert_eq!(chunk_data.next(), None);
+                            }
+
+                            assert_eq!(data.next(), None);
+                        }
+                        unknown => panic!("unexpected field {:?}", unknown),
+                    }
                 }
+
+                assert!(normal_done);
+                assert!(text_done);
+                assert!(binary_done);
+
+                tx.try_send(()).unwrap();
+
+                Response::builder().status(StatusCode::OK).body(Body::empty())
             }
-
-            assert!(normal_done);
-            assert!(text_done);
-            assert!(binary_done);
-
-            Response::builder().status(StatusCode::OK).body(Body::empty())
         }))
     });
 
@@ -96,20 +105,19 @@ async fn test_hyper_with_curl() {
     thread::spawn(move || {
         let mut form = Form::new();
         form.part("normal").contents("Hello, world!".as_bytes()).add().unwrap();
-        form.part("text-field").contents("Hello, world from a text file!".as_bytes())
-            .filename("text-file.txt")
+        form.part("text-field")
+            .buffer("text-file.txt", "Hello, world from a text file!".into())
             .content_type("text/plain")
             .add().unwrap();
 
-        let contents = (0 ..= 255).collect::<Vec<u8>>();
+        let contents = (0..=255).collect::<Vec<u8>>();
 
         form.part("binary-field")
-            .contents(&contents)
-            .filename("binary-file.bin")
+            .buffer("binary-file.bin", contents)
             .content_type("application/octet-stream")
             .add().unwrap();
 
-        let mut easy = Easy2::new(DummyHandler);
+        let mut easy = Easy2::new(TestHandler);
         easy.url(&format!("http://{}/multipart-upload", bind_addr)).unwrap();
         easy.post(true).unwrap();
         easy.httppost(form).unwrap();
@@ -117,5 +125,5 @@ async fn test_hyper_with_curl() {
         easy.perform().unwrap();
     });
 
-    server.await.unwrap()
+    server.with_graceful_shutdown(rx.next().map(|res| ())).await.unwrap()
 }
