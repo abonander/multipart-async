f7950745a0fddab3eb4f0d65ed184b2edb18328c
Austin Bonander <austin@launchbadge.com>
2019-09-30 12:41:18 -0700

    finish basic streaming client API

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -3,4 +3,39 @@
 //!
 //! Use this when sending POST requests with files to a server.
 
+use http::HeaderValue;
+use rand::distributions::{Alphanumeric, Distribution};
+use tokio_io::AsyncWrite;
+
+use crate::client::streaming::MultipartWriter;
+
 pub mod streaming;
+
+const BOUNDARY_LEN: usize = 32;
+
+pub struct MultipartRequest {
+    boundary: String,
+}
+
+impl MultipartRequest {
+    /// Start building a new `multipart/form-data` request.
+    pub fn new() -> Self {
+        let mut boundary = String::with_capacity(BOUNDARY_LEN);
+        boundary.extend(Alphanumeric.sample_iter(rand::thread_rng()).take(BOUNDARY_LEN));
+
+        MultipartRequest {
+            boundary
+        }
+    }
+
+    /// Get the value of the `Content-Type` header to be sent to the server.
+    pub fn get_content_type(&self) -> HeaderValue {
+        format!("multipart/form-data; boundary={}", self.boundary)
+            .parse().expect("this should be a valid header value")
+    }
+
+    /// Wrap a `AsyncWrite` impl.
+    pub fn wrap_writer<W: AsyncWrite + Unpin>(self, writer: W) -> MultipartWriter<W> {
+        MultipartWriter::new(writer, self.boundary)
+    }
+}
--- src/client/streaming.rs
+++ src/client/streaming.rs
@@ -1,16 +1,15 @@
-use std::io::{self, Cursor};
-use std::pin::Pin;
-
-use http::header::HeaderName;
-use http::HeaderValue;
-use mime::Mime;
-use rand::distributions::{Alphanumeric, Distribution};
-
-use tokio_io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
-use std::task::{Poll, Context};
+use std::error::Error;
 use std::future::Future;
+use std::io::{self, Cursor};
+use std::path::Path;
+use std::pin::Pin;
+use std::task::{Context, Poll};
 
-const BOUNDARY_LEN: usize = 32;
+use futures_core::Stream;
+use futures_util::TryStreamExt;
+use http::header::HeaderName;
+use mime::Mime;
+use tokio_io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 
 pub struct MultipartWriter<W> {
     inner: W,
@@ -19,20 +18,12 @@ pub struct MultipartWriter<W> {
 }
 
 impl<W> MultipartWriter<W> {
-    pub fn new(inner: W) -> Self {
-        let mut boundary = String::with_capacity(BOUNDARY_LEN);
-        boundary.extend(Alphanumeric.sample_iter(rand::thread_rng()).take(BOUNDARY_LEN));
-
+    pub (crate) fn new(inner: W, boundary: String) -> Self {
         MultipartWriter {
             inner,
             boundary,
-            data_written: false,
+            data_written: false
         }
-    }
-
-    pub fn get_content_type(&self) -> HeaderValue {
-        format!("multipart/form-data; boundary={}", self.boundary)
-            .parse().expect("this should be a valid header value")
     }
 
     fn get_field_header(&self, name: &str, filename: Option<&str>, content_type: Option<&Mime>) -> String {
@@ -52,22 +43,96 @@ impl<W> MultipartWriter<W> {
 
         header
     }
+
+    pub fn get_ref(&self) -> &W {
+        &self.inner
+    }
+
+    pub fn get_mut(&mut self) -> &mut W {
+        &mut self.inner
+    }
+
+    pub fn into_inner(self) -> W {
+        self.inner
+    }
 }
 
 impl<W: AsyncWrite + Unpin> MultipartWriter<W> {
-    async fn write_field<R: AsyncRead + Unpin>(&mut self, name: &str, filename: Option<&str>, content_type: Option<&Mime>, mut contents: R) -> io::Result<&mut Self> {
+    async fn write_field_header(&mut self, name: &str, filename: Option<&str>, content_type: Option<&Mime>) -> io::Result<()> {
         let mut header = Cursor::new(self.get_field_header(name, filename, content_type));
         header.copy(&mut self.inner).await?;
         self.data_written = true;
+        Ok(())
+    }
+
+    /// Write a field of any type to the output. (Method for taking `AsyncRead`).
+    ///
+    /// If `content_type` is not set, the server assumes `Content-Type: text/plain`
+    /// ([RFC 7578 Section 4.4][7578-4.4]). Typically, the server will interpret it as a
+    /// non-file text field like with `application/x-www-form-urlencoded` form fields.
+    ///
+    /// If this field is meant to be interpreted as a file regardless of type, then pass
+    /// `mime::APPLICATION_OCTET_STREAM`.
+    ///
+    /// `filename` is optional in all cases and may be ignored by the server if `content_type`
+    /// is not set, or may cause the server to interpret the field as a text file instead
+    /// of a text field.
+    ///
+    /// [7578-4.4]: https://tools.ietf.org/html/rfc7578#section-4.4
+    pub async fn write_field<R: AsyncRead + Unpin>(&mut self, name: &str, filename: Option<&str>, content_type: Option<&Mime>, mut contents: R) -> io::Result<&mut Self> {
+        self.write_field_header(name, filename, content_type).await?;
         contents.copy(&mut self.inner).await?;
         self.inner.write_all(b"\r\n").await?;
         Ok(self)
     }
 
+    /// Like `.write_field()` but takes a `Stream`. See that method for details on these parameters.
+    ///
+    /// Errors from the stream will be wrapped as `io::ErrorKind::Other`.
+    pub async fn write_stream<B, E, S>(&mut self, name: &str, filename: Option<&str>, content_type: Option<&Mime>, mut contents: S) -> io::Result<&mut Self>
+    where B: AsRef<[u8]>, E: Into<Box<dyn Error + Send + Sync>>, S: Stream<Item = Result<B, E>> + Unpin {
+        let mut contents = contents.map_err(|e| io::Error::new(io::ErrorKind::Other, e));
+
+        while let Some(buf) = contents.try_next().await? {
+            self.inner.write_all(buf.as_ref()).await?;
+        }
+
+        self.inner.write_all(b"\r\n").await?;
+        Ok(self)
+    }
+
+    /// Open a file for reading and copy it as a field to the output, inferring the filename
+    /// and content-type from the path.
+    ///
+    /// If no content-type is known for the path extension or there is no extension,
+    /// `application/octet-stream` is assumed to ensure the server interprets this field as a file.
+    ///
+    /// If you want to override the filename or content-type, use `.write_field()` instead.
+    #[cfg(feature = "tokio-fs")]
+    pub async fn write_file<P: AsRef<Path>>(&mut self, name: &str, path: P) -> io::Result<&mut Self> {
+        let path = path.as_ref();
+        let filename = path.file_name().and_then(|s| s.to_str());
+        let content_type = mime_guess::from_path(path).first_or_octet_stream();
+        let file = tokio_fs::File::open(path)?;
+
+        self.write_field(name, filename, Some(&content_type), file)
+    }
+
+    /// Write a plain text field to the output.
+    ///
+    /// The server must assume `Content-Type: text/plain` ([RFC 7578 Section 4.4][7578-4.4]).
+    /// Typically, the server will interpret it as a non-file text field like with
+    /// `application/x-www-form-urlencoded` form fields.
     pub async fn write_text(&mut self, name: &str, text: &str) -> io::Result<&mut Self> {
         self.write_field(name, None, None, text.as_bytes()).await
     }
 
+    /// Complete the `multipart/form-data` request.
+    ///
+    /// Writes the trailing boundary and flushes the output.
+    ///
+    /// The request should be closed at this point as the server must ignore all data outside
+    /// the multipart body.
     pub async fn finish(&mut self) -> io::Result<()> {
         if self.data_written {
             self.inner.write_all(b"--").await?;
